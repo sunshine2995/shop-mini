353620bb7682bbc0d78ca50ac38be0758f390395
sunshine <1017874118@qq.com>
2019-08-01 15:08:02 +0800

    add  Curtain component

---
+++ components/Curtain/Curtain.js
@@ -0,0 +1,30 @@
+// components/Curtain/Curtain.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    isShowCurtain: {
+      type: Boolean,
+      value: false,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  },
+  
+  ready: function () {
+    console.log(this.properties.isShowCurtain);
+  },
+})
