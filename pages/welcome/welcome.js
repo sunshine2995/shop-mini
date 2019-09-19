f9a5f2a408d9f5b7c800a4dbaa0876cc2abca02b
sunshine <1017874118@qq.com>
2019-09-19 14:59:06 +0800

    Revert "自定义图片"
    This reverts commit e07dde9856adaf97c5c2e80fd1cdef2479268430.

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,8 +1,6 @@
-var UserService = require('../../utils/services/UseService.js');
-
 Page({
   data: {
-    activityImgs: [],
+    
   },
 
   rechargePage() {
@@ -11,58 +9,25 @@ Page({
     })
   },
 
-  goSort() {
-    wx.switchTab({
-      url: '/pages/sort/sort',
-    })
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
   },
 
-  goPath(event) {
-    let path;
-    if (event.currentTarget.dataset.path) {
-      path = event.currentTarget.dataset.path;
-      console.log(event, path)
-      if (path === '/pages/home/home' || path === '/pages/cart/cart' || path === '/pages/sort/sort' || path === '/pages/user/user') {
-        wx.switchTab({
-          url: path,
-        })
-      } else {
-        wx.navigateTo({
-          url: path,
-        })
-      }
-    }
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
   },
 
-
-  getCustom() {
-    wx.showLoading({
-      title: '加载中',
-    })
-    UserService.getCustom()
-      .then((res) => {
-        wx.hideLoading();
-        const imgData = res.data.data;
-        imgData.forEach((item) => {
-          if (item.module_name === '广告页') {
-            this.data.activityImgs.push({ img_url: item.img_url, mini_turn_url: item.mini_turn_url });
-          }
-        });
-        this.setData({
-          activityImgs: this.data.activityImgs,
-        });
-      })
-      .catch((res) => {
-        wx.showToast({
-          title: res.data.message,
-          icon: 'none',
-          duration: 2000
-        })
-      })
-  },
-
+  /**
+   * 生命周期函数--监听页面显示
+   */
   onShow: function () {
-    this.getCustom();
+    
   },
 
   /**
