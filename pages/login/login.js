7748b96c9860e4283fc6f976dbac90fca9dce242
xinming90 <panxinming90@gmail.com>
2019-10-10 12:50:29 +0800

    refactor login.js

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,57 +1,30 @@
-// pages/login/login.js
-const http = require('../../services/http.js');
-const utilMd5 = require('../../utils/md5.js');
-const BaseUrl = http.BaseUrl;
-
-// const getTime = () => Math.floor(Date.now() / 1000);
-
-// const generateSignature = (timestamp) => {
-//   const password = `${timestamp}.Caibasi168`;
-//   return utilMd5.hexMD5(password);
-// };
-
-// const timestamp = getTime();
-// const signature = generateSignature(timestamp);
+import * as UserService from '../../services/UserService';
 
 Page({
-  data: {},
-
   login() {
     wx.login({
       success: (res) => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         if (res.code) {
-          wx.request({
-            method: 'POST',
-            url: `${BaseUrl}/wxapp/login`,
-
-            header: {
-              // 'content-type': 'application/x-www-form-urlencoded',
-              // Timestamp: timestamp,
-              // Signature: signature,
-            },
-            data: {
-              code: res.code,
-            },
-            success: (res) => {
+          UserService.login(res.code)
+            .then((res) => {
               const token = res.data.data.token;
-              // console.log(token, 'fff');
               wx.setStorageSync('token', token);
               wx.switchTab({
                 url: '../home/home',
               });
-            },
-            fail: (error) => {
+            })
+            .catch((error) => {
               wx.showToast({
                 title: error,
                 icon: 'none',
               });
-            },
-          });
+            });
         }
       },
     });
   },
+
   onShow() {
     this.login();
   },
