49cd10453cf313a8a198c79b4f1b964f743723dd
sunshine <1017874118@qq.com>
2019-10-12 14:28:28 +0800

    signature

--- services/http.js
+++ services/http.js
@@ -1,13 +1,24 @@
 import * as axios from '../libs/axios';
 import config from '../config/config';
+import utilMd5 from '../utils/md5';
 
 console.log('http.js: config: ', config);
 
 const http = axios.create(config.baseURL);
+const getTime = () => Math.floor(Date.now() / 1000);
+
+const generateSignature = (timestamp) => {
+  const key = `${timestamp}.Caibasi168`;
+  return utilMd5.hexMD5(key);
+};
 
 http.addRequestInterceptor((header) => {
   const token = wx.getStorageSync('token');
   header.Authorization = token;
+  const timestamp = getTime();
+  const signature = generateSignature(timestamp);
+  header.Timestamp = timestamp;
+  header.Signature = signature;
 });
 
 export default http;
